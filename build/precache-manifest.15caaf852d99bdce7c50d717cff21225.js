2d258924319b2ade92741695bcf65a100f244464
callmedadaxin <554091944@qq.com>
2019-06-19 17:36:49 +0800

    add build

---
+++ build/precache-manifest.15caaf852d99bdce7c50d717cff21225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8315e1fc7b85524c7dbad1d8aa12ec9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "023f1b67c8220dc6dd2f",
+    "url": "/static/css/main.740e16c3.chunk.css"
+  },
+  {
+    "revision": "c0242c1f164181fda1ba",
+    "url": "/static/js/2.a4413bbb.chunk.js"
+  },
+  {
+    "revision": "023f1b67c8220dc6dd2f",
+    "url": "/static/js/main.e126e06f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
