3279fcc3352b8534b6cefe002174668d97cbbf2f
callmedadaxin <554091944@qq.com>
2019-06-19 17:34:19 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+// import { Map, Marker, NavigationControl, InfoWindow } from "react-bmap";
+import "./App.css";
+import Map from "./Map";
+import List from "./List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/list/:place" component={List} />
+        <Route path="/" component={Map} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
